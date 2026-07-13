bb213a32dd0da8cdb013e9f5b309eb033bbaf3c9
Erick <erick92007@gmail.com>
2026-07-13 16:22:39 -0500

    Left off at Matrix multiplicatiion and finished matrix addition

--- Matrix.java
+++ Matrix.java
@@ -61,4 +61,36 @@ public class Matrix {
 
         return myMatrix;
     }
+
+    public static Matrix add(Matrix matrix1, Matrix matrix2) {
+        if (matrix1.row != matrix2.row || matrix1.col != matrix2.col) {
+            throw new IllegalArgumentException("Matrices need to have the same dimensions.");
+        }
+
+        Matrix arraySum = new Matrix(matrix1.row, matrix1.col);
+        for (int r = 0; r < matrix1.row; r++) {
+            for (int c = 0; c < matrix1.col; c++) {
+                arraySum.data[r][c] = matrix1.data[r][c] + matrix2.data[r][c];
+            }
+        }
+        return arraySum;
+    }
+
+    // Matrix A's cols must equal Matrix B's rows for matrices to be multiplied: 2x3
+    // * 3x2 ✅
+    // Multiply Matrix A row to Matrix B column: write stuff here
+    // LEFT OFF HERE
+    public static Matrix multiplication(Matrix matrixA, Matrix matrixB) {
+        if (matrixA.col != matrixB.row) {
+            throw new IllegalArgumentException("Matrix A's cols must equal Matrix B's rows.");
+        }
+        Matrix matrixProduct = new Matrix(matrixA.row, matrixB.col);
+        for (int r = 0; r < matrixA.row; r++) {
+            for (int c = 0; c < matrixB.col; c++) {
+                matrixProduct.data[r][c] = matrixA.data[r][c] * matrixB.data[r][c];
+            }
+        }
+        return matrixProduct;
+    }
+
 }
